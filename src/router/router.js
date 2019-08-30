1beddd4ee95499cbd092f7d6826b63bf340e57ee
unknown <619165002@qq.com>
2019-08-30 16:12:45 +0800

    产品详情完成

--- src/router/router.js
+++ src/router/router.js
@@ -2,23 +2,25 @@ import Vue from 'vue'
 import Router from 'vue-router'
 Vue.use(Router)
 
-const Home = resolve => require(['../views/Home.vue'],resolve);
-const utilPage = resolve => require(['../views/utilPage.vue'],resolve);
-const HomeEnglish = resolve => require(['../views/HomeEnglish.vue'],resolve);
-const BottomBar = resolve => require(['../views/BottomBar.vue'],resolve);
+const Home = resolve => require(['../views/Home.vue'], resolve);
+const utilPage = resolve => require(['../views/utilPage.vue'], resolve);
+const HomeEnglish = resolve => require(['../views/HomeEnglish.vue'], resolve);
+const BottomBar = resolve => require(['../views/BottomBar.vue'], resolve);
+const ProductDetail = resolve => require(['../views/ProductDetail.vue'], resolve);
 
 
 
 
 var routerMaps = [
-    {name:'Home',path:'/home',component:Home,meta: { name:'home' }},
-    {name:'HomeEnglish',path:'/homeEnglish',component:HomeEnglish,meta:{ name:'homeEnglish'}},
+    { name: 'Home', path: '/home', component: Home, meta: { name: 'home' } },
+    { name: 'HomeEnglish', path: '/homeEnglish', component: HomeEnglish, meta: { name: 'homeEnglish' } },
     // {name:'utilPage',path:'/utilPage',component:utilPage,meta: { name:'utilPage' }},
-    {name:'BottomBar', path:'/BottomBar',component:BottomBar,meta:{name:'bottomBar'}},
+    { name: 'BottomBar', path: '/BottomBar', component: BottomBar, meta: { name: 'bottomBar' } },
+    { name: 'ProductDetail', path: '/ProductDetail', component: ProductDetail, meta: { name: 'ProductDetail' } },
 ];
 
 const router = new Router({
-    routes:routerMaps
+    routes: routerMaps
 });
 
 export default router
