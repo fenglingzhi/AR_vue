aad11f3ca6fd96e296b1abc03621f581fcc54ee2
yanmeng <meng.yan@kapeixi.com>
2019-09-02 10:44:04 +0800

    merge

--- src/api/commonApi.js
+++ src/api/commonApi.js
@@ -6,13 +6,7 @@ export function Login(username,pwd) {
     };
     return postData('/login',req)
 }
-export function getCategoryList() {
-    let req = {
-        // username:username,
-        // password:pwd
-    };
-    return postData('/api/category/getCategoryProducts',req)
-}
+
 // import Vue from 'vue'
 // import VueJsonp  from "vue-jsonp";
 // Vue.use(VueJsonp);
--- src/views/category.vue
+++ src/views/category.vue
@@ -1,11 +1,40 @@
 <template>
     <div class="category">
-        <div class="category_index">
-            <ul>
-                <li>
-
-                </li>
-            </ul>
+        <header>
+            <div class="header_wrap">
+                <div class="search" onclick="search()"><img src="../assets/img/index/serch.png" alt=""></div>
+                <div class="title"><img src="../assets/img/index/AR-logo.png" alt=""></div>
+                <div class="announce" onclick="announce()"><img src="../assets/img/index/message.png" alt=""></div>
+            </div>
+        </header>
+        <div class="category_wrap">
+            <van-row>
+                <van-col span="16">
+                    <div class="menu_left">
+                        <!--<div class="menu_left_img"><img src="../assets/img/category/ab20515_0794(1).png" alt=""></div>-->
+                        <div class="level_second_list" >
+                            <!--<div class="level_second_title">تتسانستم</div>-->
+                            <div class="level_third_list_wrap">
+                                <van-row  class="level_third_list">
+                                    <van-col span="12"  class="level_third_list_item" v-for="(v,i) in category_second_list">
+                                        <img :src=v.category_url alt="">
+                                        <p :name="v.category_id">{{v.name}}</p>
+                                    </van-col>
+                                </van-row>
+                            </div>
+                        </div>
+                    </div>
+                </van-col>
+                <van-col span="8">
+                    <div class="menu_right" v-for="(v,i) in category_list">
+                        <div class="level_one_list"
+                             :class="{level_one_active:i==class_show}"
+                             @click="changeCategory(i,v.category_id)"
+                             >{{v.name}}</div>
+                    </div>
+                </van-col>
+            </van-row>
+            <div style="clear: both"></div>
         </div>
     </div>
 </template>
@@ -22,19 +51,26 @@
         components: {
             router,
             store,
-            getData,
-            postData
         },
         data() {
             return {
-                title1:'en',
-                title2:'ar'
+                category_list:{},
+                category_second_list:{},
+                class_show:0
             }
         },
         methods: {
             getCategoryList(data){
                 this.$post('/api/navigation/getlists',{id_currency:1}).then(resp => {
-                    console.log(resp)
+                    this.category_list = resp.data;
+                    // console.log(this.category_list)
+                })
+            },
+            changeCategory(i,id){
+                this.class_show = i;
+                this.$post('/api/navigation/getSecondLists',{id_category:id}).then(resp => {
+                    this.category_second_list = resp.data;
+                    console.log(this.category_second_list)
                 })
             }
         },
@@ -44,8 +80,78 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    p{
+        margin: 0;
+    }
+    img{
+        width: 100%;
+        height: 100%;
+    }
     .category{
-
+        header{
+            position: fixed;
+            overflow: auto;
+            background: #fff;
+            top: 0;
+            width: 100%;
+            border-bottom: 1px solid #E7E7E7;
+            .header_wrap{
+                height: 44px;
+                display: flex;
+                display:-webkit-flex;
+                justify-content:space-between;
+                padding: 0 10px;
+                align-items: center;
+            }
+        }
+        .category_wrap{
+            margin-top: 44px;
+            padding-bottom: 67px;
+        }
+        .menu_right{
+            width: 100%;
+            text-align: right;
+            background: #F6f6f6;
+            float: right;
+        }
+        .level_one_list{
+            padding: 10px 20px;
+            color: #999;
+            border-bottom: 1px solid #D8D8D8;
+        }
+        .level_one_active{
+            background: #fff;
+            border-right: 2px solid #000;
+            font-weight: 600;
+            color: #000;
+        }
+        .menu_left{
+            float: left;
+            overflow: auto;
+            /*padding: 0 10px;*/
+        }
+        .menu_left img{
+            height: auto;
+        }
+        .level_second_title{
+            text-align: right;
+            font-weight: bold;
+            margin: 10px 0;
+        }
+        .level_third_list{
+            margin-top: 10px;
+            clear: both;
+            overflow: auto;
+        }
+        .level_third_list p{
+            text-align: center;
+        }
+        .level_third_list_item{
+            /*width: 33.3333%;*/
+            float: right;
+            margin-bottom: 10px;
+            padding: 0 5px;
+        }
     }
 </style>
