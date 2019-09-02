3737e78eab146ec39a096430d882a9f49959977f
unknown <619165002@qq.com>
2019-09-02 15:59:15 +0800

    解决router冲突

--- src/router/router.js
+++ src/router/router.js
@@ -5,9 +5,7 @@ Vue.use(Router)
 const Home = resolve => require(['../views/Home.vue'],resolve);
 // const utilPage = resolve => require(['../views/utilPage.vue'],resolve);
 const BottomBar = resolve => require(['../views/BottomBar.vue'],resolve);
-const SearchHot = resolve => require(['../components/SearchHot.vue'],resolve);
 const SearchPage = resolve => require(['../components/SearchPage.vue'],resolve);
-const Category = resolve => require(['../views/category.vue'],resolve);
 const PersonHistoryOrder = resolve => require(['../components/PersonHistoryOrder.vue'],resolve);
 const PersonOrderDetail = resolve => require(['../components/PersonOrderDetail.vue'],resolve);
 const ProductDetail = resolve => require(['../views/ProductDetail.vue'], resolve);
@@ -24,9 +22,7 @@ var routerMaps = [
     { name: 'Home', path: '/home', component: Home, meta: { name: 'home' } },
     // {name:'utilPage',path:'/utilPage',component:utilPage,meta: { name:'utilPage' }},
     {name:'BottomBar', path:'/BottomBar',component:BottomBar,meta:{name:'bottomBar'}},
-    {name:'SearchHot', path:'/SearchHot',component:SearchHot,meta:{name:'searchHot'}},
     {name:'SearchPage', path:'/SearchPage',component:SearchPage,meta:{name:'searchPage'}},
-    {name:'Category', path:'/Category',component:Category,meta:{name:'Category'}},
     {name:'PersonHistoryOrder', path:'/PersonHistoryOrder',component:PersonHistoryOrder,meta:{name:'personHistoryOrder'}},
     {name:'PersonOrderDetail', path:'/PersonOrderDetail',component:PersonOrderDetail,meta:{name:'personOrderDetail'}},
     { name: 'ProductDetail', path: '/ProductDetail', component: ProductDetail, meta: { name: 'ProductDetail' } },
