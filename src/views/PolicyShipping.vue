cfb030c4be062fc628e289a9ef210265964791ff
unknown <619165002@qq.com>
2019-09-09 13:26:17 +0800

    完成shipping/return页面

--- src/views/PolicyShipping.vue
+++ src/views/PolicyShipping.vue
@@ -30,7 +30,7 @@
           <p :class="{'active':faq_type==2}" @click="faq_type=2">دول الخليج العربية الست</p>
         </div>
         <div class="type-detail" v-if="faq_type==1">
-          <van-collapse v-model="activeNames1">
+          <van-collapse v-model="activeNames1" accordion>
             <van-collapse-item title="الولايات المتحدة" name="1-1">
               <table>
                 <tbody>
@@ -58,20 +58,262 @@
                   </tr>
                 </tbody>
               </table>
+              <div>
+                <p>ملاحظة:</p>
+                <p>لم نفتح بعد قنوات لوجستية في ألاسكا وساموا الأمريكية وغوام وهاواي وجزر ماريانا الشمالية وبورتوريكو وجزر فيرجن، لذلك لن يتم شحن المنتجات إلى هذه المناطق مؤقتا.</p>
+              </div>
             </van-collapse-item>
-            <van-collapse-item title="标题2" name="1-2">内容</van-collapse-item>
-            <van-collapse-item title="标题2" name="1-3">内容</van-collapse-item>
-            <van-collapse-item title="标题2" name="1-4">内容</van-collapse-item>
+            <van-collapse-item title="أستراليا" name="1-2">
+              <table>
+                <tbody>
+                  <tr style="height: 21px;">
+                    <td style="width: 33.3333%; text-align: center; height: 21px;">طريق الشحن</td>
+                    <td style="width: 33.3333%; text-align: center; height: 21px;">التكلفة</td>
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >زمن التوصيل (يوم العمل)</td>
+                  </tr>
+                  <tr style="height: 21px;">
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 42px;"
+                      rowspan="2"
+                    >الشحن المعجل</td>
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >الشحن المجاني للطلب أكثر من $40</td>
+                    <td style="width: 33.3333%; text-align: center; height: 42px;" rowspan="2">8~6</td>
+                  </tr>
+                  <tr style="height: 21px;">
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >رسم الشحن $3 للطلب $0-$40</td>
+                  </tr>
+                </tbody>
+              </table>
+            </van-collapse-item>
+            <van-collapse-item title="كندا" name="1-3">
+              <table>
+                <tbody>
+                  <tr style="height: 21px;">
+                    <td style="width: 33.3333%; text-align: center; height: 21px;">طريق الشحن</td>
+                    <td style="width: 33.3333%; text-align: center; height: 21px;">التكلفة</td>
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >زمن التوصيل (يوم العمل)</td>
+                  </tr>
+                  <tr style="height: 21px;">
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 42px;"
+                      rowspan="2"
+                    >الشحن المعجل</td>
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >الشحن المجاني للطلب أكثر من $40</td>
+                    <td style="width: 33.3333%; text-align: center; height: 42px;" rowspan="2">8~6</td>
+                  </tr>
+                  <tr style="height: 21px;">
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >رسم الشحن $4 للطلب $0-$40</td>
+                  </tr>
+                </tbody>
+              </table>
+            </van-collapse-item>
+            <van-collapse-item title="الدول الأخرى" name="1-4">
+              <table>
+                <tbody>
+                  <tr style="height: 21px;">
+                    <td style="width: 33.3333%; text-align: center; height: 21px;">طريق الشحن</td>
+                    <td style="width: 33.3333%; text-align: center; height: 21px;">التكلفة</td>
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >زمن التوصيل (يوم العمل)</td>
+                  </tr>
+                  <tr style="height: 21px;">
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 42px;"
+                      rowspan="2"
+                    >الشحن السريع</td>
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >رسم الشحن $15 لجميع الطلبات</td>
+                    <td style="width: 33.3333%; text-align: center; height: 42px;" rowspan="2">8~4</td>
+                  </tr>
+                </tbody>
+              </table>
+            </van-collapse-item>
           </van-collapse>
         </div>
         <div class="type-detail" v-if="faq_type==2">
-          <van-collapse v-model="activeNames2">
-            <van-collapse-item title="标题1" name="2-1">内容</van-collapse-item>
-            <van-collapse-item title="标题2" name="2-2">内容</van-collapse-item>
-            <van-collapse-item title="标题2" name="2-3">内容</van-collapse-item>
-            <van-collapse-item title="标题2" name="2-4">内容</van-collapse-item>
-            <van-collapse-item title="标题2" name="2-5">内容</van-collapse-item>
-            <van-collapse-item title="标题2" name="2-6">内容</van-collapse-item>
+          <van-collapse v-model="activeNames2" accordion>
+            <van-collapse-item title="الإمارات" name="2-1">
+              <table>
+                <tbody>
+                  <tr style="height: 21px;">
+                    <td style="width: 33.3333%; text-align: center; height: 21px;">طريق الشحن</td>
+                    <td style="width: 33.3333%; text-align: center; height: 21px;">التكلفة</td>
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >زمن التوصيل (يوم العمل)</td>
+                  </tr>
+                  <tr style="height: 21px;">
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 42px;"
+                      rowspan="2"
+                    >الشحن المعجل</td>
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >الشحن المجاني للطلب أكثر من $50</td>
+                    <td style="width: 33.3333%; text-align: center; height: 42px;" rowspan="2">7~3</td>
+                  </tr>
+                  <tr style="height: 21px;">
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >رسم الشحن $8 للطلب $0-$50</td>
+                  </tr>
+                </tbody>
+              </table>
+            </van-collapse-item>
+            <van-collapse-item title="السعودية" name="2-2">
+              <table>
+                <tbody>
+                  <tr style="height: 21px;">
+                    <td style="width: 33.3333%; text-align: center; height: 21px;">طريق الشحن</td>
+                    <td style="width: 33.3333%; text-align: center; height: 21px;">التكلفة</td>
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >زمن التوصيل (يوم العمل)</td>
+                  </tr>
+                  <tr style="height: 21px;">
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 42px;"
+                      rowspan="2"
+                    >الشحن السريع</td>
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >الشحن المجاني للطلب أكثر من $89</td>
+                    <td style="width: 33.3333%; text-align: center; height: 42px;" rowspan="2">8~3</td>
+                  </tr>
+                  <tr style="height: 21px;">
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >رسم الشحن $8 للطلب $0-$89</td>
+                  </tr>
+                </tbody>
+              </table>
+            </van-collapse-item>
+            <van-collapse-item title="الكويت" name="2-3">
+              <table>
+                <tbody>
+                  <tr style="height: 21px;">
+                    <td style="width: 33.3333%; text-align: center; height: 21px;">طريق الشحن</td>
+                    <td style="width: 33.3333%; text-align: center; height: 21px;">التكلفة</td>
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >زمن التوصيل (يوم العمل)</td>
+                  </tr>
+                  <tr style="height: 21px;">
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 42px;"
+                      rowspan="2"
+                    >الشحن السريع</td>
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >الشحن المجاني للطلب أكثر من $99</td>
+                    <td style="width: 33.3333%; text-align: center; height: 42px;" rowspan="2">8~3</td>
+                  </tr>
+                  <tr style="height: 21px;">
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >رسم الشحن $8 للطلب $0-$99</td>
+                  </tr>
+                </tbody>
+              </table>
+            </van-collapse-item>
+            <van-collapse-item title="البحرين" name="2-4">
+              <table>
+                <tbody>
+                  <tr style="height: 21px;">
+                    <td style="width: 33.3333%; text-align: center; height: 21px;">طريق الشحن</td>
+                    <td style="width: 33.3333%; text-align: center; height: 21px;">التكلفة</td>
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >زمن التوصيل (يوم العمل)</td>
+                  </tr>
+                  <tr style="height: 21px;">
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 42px;"
+                      rowspan="2"
+                    >الشحن السريع</td>
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >الشحن المجاني للطلب أكثر من $99</td>
+                    <td style="width: 33.3333%; text-align: center; height: 42px;" rowspan="2">8~3</td>
+                  </tr>
+                  <tr style="height: 21px;">
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >رسم الشحن $8 للطلب $0-$99</td>
+                  </tr>
+                </tbody>
+              </table>
+            </van-collapse-item>
+            <van-collapse-item title="عمان" name="2-5">
+              <table>
+                <tbody>
+                  <tr style="height: 21px;">
+                    <td style="width: 33.3333%; text-align: center; height: 21px;">طريق الشحن</td>
+                    <td style="width: 33.3333%; text-align: center; height: 21px;">التكلفة</td>
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >زمن التوصيل (يوم العمل)</td>
+                  </tr>
+                  <tr style="height: 21px;">
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 42px;"
+                      rowspan="2"
+                    >الشحن السريع</td>
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >الشحن المجاني للطلب أكثر من $89</td>
+                    <td style="width: 33.3333%; text-align: center; height: 42px;" rowspan="2">8~3</td>
+                  </tr>
+                  <tr style="height: 21px;">
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >رسم الشحن $8 للطلب $0-$89</td>
+                  </tr>
+                </tbody>
+              </table>
+            </van-collapse-item>
+            <van-collapse-item title="قطر" name="2-6">
+              <table>
+                <tbody>
+                  <tr style="height: 21px;">
+                    <td style="width: 33.3333%; text-align: center; height: 21px;">طريق الشحن</td>
+                    <td style="width: 33.3333%; text-align: center; height: 21px;">التكلفة</td>
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >زمن التوصيل (يوم العمل)</td>
+                  </tr>
+                  <tr style="height: 21px;">
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 42px;"
+                      rowspan="2"
+                    >الشحن السريع</td>
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >الشحن المجاني للطلب أكثر من $99</td>
+                    <td style="width: 33.3333%; text-align: center; height: 42px;" rowspan="2">8~3</td>
+                  </tr>
+                  <tr style="height: 21px;">
+                    <td
+                      style="width: 33.3333%; text-align: center; height: 21px;"
+                    >رسم الشحن $8 للطلب $0-$99</td>
+                  </tr>
+                </tbody>
+              </table>
+            </van-collapse-item>
           </van-collapse>
         </div>
         <div class="tip">
@@ -93,8 +335,8 @@ export default {
   data() {
     return {
       faq_type: 1,
-      activeNames1: ["1-1", "1-2", "1-3", "1-4"],
-      activeNames2: ["2-1", "2-2", "2-3", "2-4", "2-5", "2-6"]
+      activeNames1: "1-1",
+      activeNames2: "2-1"
     };
   },
   components: {},
@@ -163,17 +405,22 @@ export default {
         }
       }
       .type-detail {
-        .van-collapse-item{
+        .van-collapse-item {
           margin-bottom: 5px;
         }
-        .van-collapse-item__title{
+        .van-collapse-item__title {
           background: #d8d8d8;
         }
-        .van-collapse-item__content{
-          padding:0;
+        .van-collapse-item__content {
+          padding: 0;
+          color: #000000;
         }
-        table{
-          border:1px solid #999999;
+        table {
+          border-collapse: collapse;
+          td {
+            border: 1px solid #999999;
+            padding: 5px;
+          }
         }
       }
     }
